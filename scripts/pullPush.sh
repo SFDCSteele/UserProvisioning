1fdf45548073053676559a096bdad929fbaf7eba
William Steele <williamsteele@Williams-iMac.local>
2021-02-03 17:43:17 -0500

    platform event updates

--- scripts/pullPush.sh
+++ scripts/pullPush.sh
@@ -2,7 +2,7 @@
 
 export dxOrg="UserProvisioningAppScratch"
 export dxUser="test-fudubkdhqeya@example.com"
-export gitBranch="serProvisioning"
+export gitBranch="UserProvisioning"
 
 echo "==========================================================================="
 echo "Pulling changes from $dxOrg as user $dxUser and pushing to branch $gitBranch with the comment $1"
