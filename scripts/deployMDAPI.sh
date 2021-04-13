107e10c997e0f428d2661e1742e292d5f3b23b09
SFDCSteele <wsteele@salesforce.com>
2021-04-13 15:56:56 -0400

    Source format converted to MDAPI

---
+++ scripts/deployMDAPI.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export dxOrg="CoreFunctionalityAppScratch"
+export dxUser="test-bwqfp9xky4fo@example.com"
+export MDAPIUser="CoreFunctionality"
+export gitBranch="CoreFunctionality"
+
+echo "==========================================================================="
+echo "Setting up Scratch Org $dxOrg as user $dxUser and pushing to branch $gitBranch to the Org"
+echo "==========================================================================="
+
+git pull origin $gitBranch
+
+rm -rf ./src
+echo "MDAPI source removed..."
+sfdx force:source:convert \
+    -d ./src \
+    -n "$gitBranch"
+
+ls -al
+echo "MDAPI source converted from source format..."
+read inpt
+
+if [ "$1" = "Y" ] 
+then
+    sfdx auth:web:login -d -a $MDAPIUser
+fi
+
+
+sfdx force:mdapi:deploy -u $gitBranch -w 10 -d ./src
+#read inpt
+
+git status
+git add .
+git commit -m "Source format converted to MDAPI"
+git push origin $gitBranch
+
+echo "==========================================================================="
+sfdx force:org:list
+sfdx force:source:status
+echo "==========================================================================="
+
+echo "Reset org $dxOrg processing complete....."
