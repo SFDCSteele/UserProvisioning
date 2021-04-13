ceff160f2a53bc7350a0d07528dd1489950d1ccc
SFDCSteele <wsteele@salesforce.com>
2021-04-13 15:16:21 -0400

    Source format converted to MDAPI

---
+++ scripts/deployMDAPI.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export dxOrg="ActiveDirectoryAppScratch"
+export dxUser="test-p9yeswr0dvzw@example.com"
+export MDAPIUser="ActiveDirectory"
+export gitBranch="ActiveDirectory"
+
+echo "==========================================================================="
+echo "Setting up Scratch Org $dxOrg as user $dxUser and pushing to branch $gitBranch to the Org"
+echo "==========================================================================="
+
+git pull origin $gitBranch
+
+rm -rf ./src
+echo "MDAPI source removed..."
+sfdx force:source:convert \
+    -d ./src \
+    -n "$gitBranch"
+
+ls -al
+echo "MDAPI source converted from source format..."
+#read inpt
+
+sfdx force:mdapi:deploy -u $gitBranch -w 10 -d ./src
+#read inpt
+
+git status
+git add .
+git commit -m "Source format converted to MDAPI"
+git push origin $gitBranch
+
+echo "==========================================================================="
+sfdx force:org:list
+sfdx force:source:status
+echo "==========================================================================="
+
+echo "Reset org $dxOrg processing complete....."
---
+++ src/applications/Active_Directory_Manager.app
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<CustomApplication xmlns="http://soap.sforce.com/2006/04/metadata">
+    <brand>
+        <headerColor>#0070D2</headerColor>
+        <shouldOverrideOrgTheme>false</shouldOverrideOrgTheme>
+    </brand>
+    <formFactors>Small</formFactors>
+    <formFactors>Large</formFactors>
+    <isNavAutoTempTabsDisabled>false</isNavAutoTempTabsDisabled>
+    <isNavPersonalizationDisabled>false</isNavPersonalizationDisabled>
+    <label>Active Directory Manager</label>
+    <navType>Standard</navType>
+    <tabs>AD_User__c</tabs>
+    <uiType>Lightning</uiType>
+    <utilityBar>Active_Directory_Manager_UtilityBar</utilityBar>
+</CustomApplication>
