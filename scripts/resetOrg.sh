e3a7b4659ca04b3e99ca933971d1598c835a2b04
SFDCSteele <wsteele@salesforce.com>
2021-04-15 11:34:23 -0400

    Updating config and build files

--- scripts/resetOrg.sh
+++ scripts/resetOrg.sh
@@ -8,21 +8,26 @@ echo "==========================================================================
 echo "Setting up Scratch Org $dxOrg as user $dxUser and pushing to branch $gitBranch to the Org"
 echo "==========================================================================="
 
-#sfdx auth:web:login -d -a DevHub
-#echo "Once DevHub is authorized, hit return to continue..."
-#read inpt
-
 if [ "$1" = "Y" ]
+then
+    sfdx auth:web:login -d -a DevHub
+    echo "Once DevHub is authorized, hit return to continue..."
+    read inpt
+fi
+
+if [ "$2" = "Y" ]
 then
     sfdx force:org:create -s -f config/project-scratch-def.json -a $dxOrg --durationdays 30
     sfdx force:user:password:generate
-    force:org:describe
 fi
+sfdx force:org:list
+echo "Org has been created, hit return to continue..."
+read inpt
 
 git pull origin $gitBranch
 sfdx force:source:push -u $dxUser
 
-#sfdx force:org:open -u $dxUser
+sfdx force:org:open -u $dxOrg
 
 #sfdx force:data:tree:export -q "select Id,Name from Profile WHERE Name='System Administrator'" -d ./data
 #echo "Update the Profile Id AND username in the User.json file, hit return to continue..."
