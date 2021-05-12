662ceb1ad90494553f4e1c8bccec156e9849f91e
SFDCSteele <wsteele@salesforce.com>
2021-05-12 15:53:55 -0400

    Created event Apex for destination event

---
+++ scripts/deployMDAPI.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+export dxOrg="UserProvisioningAppScratch"
+export dxUser="test-hmtdsblmbaha@example.com"
+export MDAPIUser="UserProvisioning"
+export gitBranch="UserProvisioning"
+
+echo "==========================================================================="
+echo "Setting up Scratch Org $dxOrg as user $dxUser and pushing to branch $gitBranch to the Org"
+echo "==========================================================================="
+
+git pull origin $gitBranch
+
+rm -rf ./src
+echo "MDAPI source removed...pausing briefly...hit return to continue"
+read inpt
+
+sfdx force:source:convert \
+    -d ./src \
+    -n "$gitBranch"
+
+ls -al
+echo "MDAPI source converted from source format..."
+read inpt
+
+if [ "$1" = "Y" ] 
+then
+    sfdx auth:web:login -d -a $MDAPIUser
+fi
+
+
+sfdx force:mdapi:deploy -u $gitBranch -w 10 -d ./src
+echo "MDAPI source deployed...press return to continue..."
+read inpt
+
+git status
+git add .
+git commit -m "Source format converted to MDAPI"
+git push origin $gitBranch
+
+echo "==========================================================================="
+sfdx force:org:list
+sfdx force:source:status
+echo "==========================================================================="
+
+echo "Reset org $dxOrg processing complete....."
